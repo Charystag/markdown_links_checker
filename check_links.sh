7e3bb0fc5660cc7817e306273a6a69759c55cbc2
Charystag <57284562+Charystag@users.noreply.github.com>
2024-08-23 16:14:04 +0200

    Added function parse_link alongside its documentation Added docs for function get_links and check_link Need to implement the check_link function

--- check_links.sh
+++ check_links.sh
@@ -7,11 +7,14 @@ teardown(){
 	kill -INT $$
 }
 
-declare -a links
 
 :<<-GET_LINKS
 	Function to retrieve all the links in a markdown document.
-	The function retrieves the links 
+	The function retrieves the links an puts them in the links
+	array using the following form :
+	line_number:[link_text](link_url)
+	ex:
+	61:[ListView](https://doc.qt.io/qt-6/qml-qtquick-listview.html)
 GET_LINKS
 get_links(){
 	declare document="$1";
@@ -28,5 +31,42 @@ get_links(){
 	return 0;
 }
 
-get_links "$1"
-echo "${links[@]}"
+:<<-"PARSE_LINK"
+	The parse link function is meant to check the format of a link. It takes a 
+	http url as an argument and returns true if the url is a valid url and false 
+	otherwise
+
+	The url checks now if :
+	- The link begins with `http://' or `https://'
+PARSE_LINK
+parse_link(){
+	declare link="$1"
+
+	if ! { echo -n "${link}" | grep -E '^http(s)?://' ; };
+	then return 1; else return 0; fi
+}
+
+:<<-"CHECK_LINK"
+	This function performs a curl request on the link it receives 
+	if it is a valid link.
+	A variable `http_return_code' should be declared in the enclosing scopr in 
+	order for the return code to be registered. The return code will be returned 
+	as a string
+CHECK_LINK
+check_link(){
+	declare link="$1";
+
+	if ! parse_link "${link}";
+	then
+		echo "\`${link}' is not a valid http url.";
+		return;
+	fi
+}
+
+main(){
+	declare -a links;
+
+	get_links "$1";
+	echo "${links[@]}";
+}
+
