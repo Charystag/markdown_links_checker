0e70347392814b39d4381df11abcdde76f38b01a
Charystag <57284562+Charystag@users.noreply.github.com>
2024-08-23 16:52:40 +0200

    Fixed linter warnings

--- check_links.sh
+++ check_links.sh
@@ -24,12 +24,11 @@ get_links(){
 	declare document="$1";
 	declare tmp_file;
 	declare link;
-	declare -i i=0;
 
 	if [ "$document" = "" ]; then  teardown "Please provide an input document"; fi
 	if ! tmp_file="$(mktemp /tmp/check-links-script.XXXXXXXXXX)" ; then teardown "Couldn't create tmp file" ; fi
 	grep -E '\[.+\]\(.+\)' -n -o "${document}"  > "${tmp_file}";
-	while IFS='\n' read -r link;
+	while IFS=$'\n' read -r link;
 	do links+=( "${link}" ); done < "${tmp_file}"
 	rm "$tmp_file";
 	return 0;
@@ -77,6 +76,7 @@ check_link(){
 	red
 CHECK_LINKS
 check_links(){
+	true
 }
 
 main(){
