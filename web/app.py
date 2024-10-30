216c2a27b80ae2aacfe623644804070f2c26a301
Charystag <charystag@proton.me>
2024-10-30 16:14:10 +0100

    Changes to be committed: 	modified:   web/app.py 	Started adding methods to act on the data 	new file:   web/interface.py 	Started adding file to help the code from Python and from Bash interact

--- web/app.py
+++ web/app.py
@@ -1,7 +1,19 @@
 from flask import Flask
+from markupsafe import escape
+import os
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/delete")
+def delete():
+    try:
+        #os.execl("/bin/rm", "-rf test")
+        os.rmdir("test")
+    except Exception:
+        return "<p>The test folder couldn't be deleted due to exception </p>"
+    return "<p>The test folder has been deleted</p>"
